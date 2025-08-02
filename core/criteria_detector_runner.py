5eed4bc86d7168dbaf2643497c8f2dbc47c51cb1
igorlapin <igorlapin6@gmail.com>
2025-08-02 12:27:46 +0300

    add await request detector

--- core/criteria_detector_runner.py
+++ core/criteria_detector_runner.py
@@ -9,7 +9,7 @@ from core.repository.entity.dialog_rows import DialogRow
 from core.service.dialog_row_util_service import print_dialog, print_dialog_with_row_text
 
 
-def chunk_list(rows: List[DialogRow], chunk_size: int = 500) -> List[List[DialogRow]]:
+def chunk_list(rows: List[DialogRow], chunk_size: int = 300) -> List[List[DialogRow]]:
     return [rows[i:i + chunk_size] for i in range(0, len(rows), chunk_size)]
 
 if __name__ == "__main__":
@@ -18,9 +18,11 @@ if __name__ == "__main__":
     dialog_criteria_repository = DialogCriteriaRepository()
 
     # rows = dialog_rows_repository.find_by_dialog_id(uuid.UUID('b6444029-efbd-4994-9f7b-3e07022fa386'))
-    rows = dialog_rows_repository.find_all()
+    # rows = dialog_rows_repository.find_all()
+    rows = dialog_rows_repository.find_rows_without_criteria()
     for chunk in chunk_list(rows):
         result = process_rows_parallel(chunk)
+        print(f"saved {len(result)} rows")
         dialog_criteria_repository.update_all_criteria(result)
     # dialogs = audio_dialog_repository.find_all()
     # for dialog in dialogs:
